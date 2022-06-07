5670935dc2c1febcf0615072726a2f40cedb32c1
Arthur <boredarthur@gmail.com>
2022-06-07 14:33:05 +0300

    webelement wrappers

---
+++ src/main/java/pageFactory/Wrappers/Element.java
@@ -0,0 +1,8 @@
+package main.java.pageFactory.Wrappers;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WrapsElement;
+import org.openqa.selenium.interactions.Locatable;
+
+public interface Element extends WebElement, WrapsElement, Locatable {
+}
